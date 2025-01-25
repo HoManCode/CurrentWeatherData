90aedf64583f95321fb39fe17dec6d369eaec54a
Hooman Shykhipour <59225708+HoManCode@users.noreply.github.com>
2025-01-26 00:47:54 +1100

    wire backend and frontend together and create wwwroot folder

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -2,21 +2,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
 app.UseHttpsRedirection();
 
 // Serve static files from the React build folder (for production)
-app.UseStaticFiles(); 
+app.UseDefaultFiles();
+//app.UseStaticFiles(); 
 
 // Set up the development proxy for React (if in development mode)
 if (app.Environment.IsDevelopment())
@@ -26,7 +27,7 @@ if (app.Environment.IsDevelopment())
         spa.Options.SourcePath = "Frontend"; // Path to the React app folder
 
         // Proxy to the React development server
-        spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+        spa.UseProxyToSpaDevelopmentServer("http://localhost:5173/");
     });
 }
 
--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../BackEnd/wwwroot', // Output to the wwwroot folder
+    emptyOutDir: true,
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5115', // Backend URL
+        changeOrigin: true,
+      },
+    },
+  }
 })
